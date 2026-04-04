dee6b325da5942295f32eca0af4230b3fcc7e8a7
Jinpeng Zhu <dpthinker@qq.com>
2026-04-04 13:34:50 +0800

    chore: bump release version to 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "com.mindflow.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
