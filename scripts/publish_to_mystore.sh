edf77c482267aa668e4ff6ea67c7f5d7b6d27a37
Jinpeng Zhu <dpthinker@qq.com>
2026-04-23 09:51:16 +0800

    chore: load mystore publish env from config dir

--- scripts/publish_to_mystore.sh
+++ scripts/publish_to_mystore.sh
@@ -6,6 +6,15 @@ MYSTORE_ROOT="${MYSTORE_ROOT:-/home/dpthinker/MyStore}"
 ANDROID_HOME="${ANDROID_HOME:-/home/dpthinker/Android/Sdk}"
 ANDROID_SDK_ROOT="${ANDROID_SDK_ROOT:-$ANDROID_HOME}"
 JAVA_HOME="${JAVA_HOME:-$HOME/.jdks/temurin-21}"
+DEFAULT_MYSTORE_ENV_FILE="$HOME/.config/mystore/publish.env"
+MYSTORE_ENV_FILE="${MYSTORE_ENV_FILE:-$DEFAULT_MYSTORE_ENV_FILE}"
+
+if [ -f "$MYSTORE_ENV_FILE" ]; then
+  set -a
+  # shellcheck disable=SC1090
+  . "$MYSTORE_ENV_FILE"
+  set +a
+fi
 
 cd "$PROJECT_ROOT"
 ANDROID_HOME="$ANDROID_HOME" ANDROID_SDK_ROOT="$ANDROID_SDK_ROOT" JAVA_HOME="$JAVA_HOME" ./gradlew --no-daemon :app:assembleRelease
